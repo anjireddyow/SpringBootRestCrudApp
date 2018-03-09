fb4531ec9c46b230566ed7a64bd4acd14938a673
sudeerone <anji.r@prodapt.com>
2018-03-08 17:00:20 -0800

    partial spring security

--- src/main/java/com/example/demo/security/config/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/security/config/SpringSecurityConfig.java
@@ -1,26 +1,61 @@
 package com.example.demo.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	
+	 private static String REALM="MY_TEST_REALM";
+     
+//	    @Autowired
+//	    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+//	        auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("ADMIN");
+//	        auth.inMemoryAuthentication().withUser("tom").password("abc123").roles("USER");
+//	    }
+//	     
+//	    @Override
+//	    protected void configure(HttpSecurity http) throws Exception {
+//	  
+//	      http.csrf().disable()
+//	        .authorizeRequests()
+//	        .antMatchers("/**").hasRole("ADMIN")
+//	        .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+//	        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
+//	    }
+//	     
+//	    @Bean
+//	    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
+//	        return new CustomBasicAuthenticationEntryPoint();
+//	    }
+//	     
+//	    /* To allow Pre-flight [OPTIONS] request from browser */
+//	    @Override
+//	    public void configure(WebSecurity web) throws Exception {
+//	        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+//	    }
+	    
 	// Authentication : User --> Roles
 		protected void configure(AuthenticationManagerBuilder auth)
 				throws Exception {
-//			auth.inMemoryAuthentication().withUser("user1").password("secret1")
-//					.roles("USER").and().withUser("admin1").password("secret1")
-//					.roles("USER", "ADMIN");
+			auth.inMemoryAuthentication().withUser("user1").password("secret1").roles("USER").
+			and().withUser("admin1").password("secret1").roles("USER", "ADMIN");
 		}
 
 		// Authorization : Role -> Access
 		protected void configure(HttpSecurity http) throws Exception {
-//			http.httpBasic().and().authorizeRequests().antMatchers("/employees**")
+//			http.httpBasic().and().authorizeRequests().antMatchers("/**")
 //					.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
 //					.csrf().disable().headers().frameOptions().disable();
+			
 		}
 	
 }
