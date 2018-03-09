c40aa5aba8b7b1046ee4f3faf6d810e362f89e51
sudeerone <anji.r@prodapt.com>
2018-03-09 09:55:43 -0800

    spring profile configuration with common properties and logging

--- src/main/java/com/example/demo/SpringBootRestCrudAppApplication.java
+++ src/main/java/com/example/demo/SpringBootRestCrudAppApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootRestCrudAppApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SpringBootRestCrudAppApplication.class);
 
 	public static void main(String[] args) {
 		ApplicationContext actx = SpringApplication.run(SpringBootRestCrudAppApplication.class, args);
+		logger.info("Welcome info log message");
 	}
 }
